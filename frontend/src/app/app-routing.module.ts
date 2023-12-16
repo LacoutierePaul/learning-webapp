8ab7f8136a26cff7b04ee8e275d2e8965955aae7
LacoutierePaul <paul.lacoutiere@gmail.com>
2023-12-16 13:46:21 +0100

    Lien vers home page et study page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {StudyPageComponent} from "./study-page/study-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
+const routes: Routes = [
+  { path:'study-page', component: StudyPageComponent },
+  { path:'', component: HomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
