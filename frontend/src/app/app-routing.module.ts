2e25186b1a5d55d337b22f05c14a11b53d5b7062
LacoutierePaul <paul.lacoutiere@gmail.com>
2023-12-17 12:06:36 +0100

    add path learning-fact

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {StudyPageComponent} from "./study-page/study-page.component";
 import {HomePageComponent} from "./home-page/home-page.component";
+import {LearningFactPageComponent} from "./learning-fact-page/learning-fact-page.component";
 const routes: Routes = [
   { path:'study-page', component: StudyPageComponent },
-  { path:'', component: HomePageComponent }
+  { path:'', component: HomePageComponent },
+  { path:'learning-fact-page/:id', component: LearningFactPageComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatCheckboxModule} from "@angular/material/checkbox";
+import { LearningFactPageComponent } from './learning-fact-page/learning-fact-page.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
     NavbarComponent,
     StudyPageComponent,
     HomePageComponent,
-    LearningPackageCardComponent
+    LearningPackageCardComponent,
+    LearningFactPageComponent
   ],
   imports: [
     BrowserModule,
