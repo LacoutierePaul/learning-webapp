ef4c057fddcd813da03df7316a83511953dd7ef9
mathiasbalian <mathias.balian92@gmail.com>
2023-12-19 19:23:17 +0100

    added feature for review session

--- frontend/src/app/learning-fact-page/learning-fact-page.component.ts
+++ frontend/src/app/learning-fact-page/learning-fact-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { LearningPackage,LearningFact } from '../app.component';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {LearningPackage, LearningFact} from '../app.component';
 import {ActivatedRoute} from "@angular/router";
 import {last} from "rxjs";
 
@@ -9,19 +9,23 @@ import {last} from "rxjs";
   templateUrl: './learning-fact-page.component.html',
   styleUrls: ['./learning-fact-page.component.css']
 })
-export class LearningFactPageComponent implements OnInit, OnDestroy{
+export class LearningFactPageComponent implements OnInit, OnDestroy {
 
   learningPackage: any;
-  learningFacts: LearningFact[] = [];
+  facts: LearningFact[] = []; // All facts including learning and review facts
+  learningFacts: LearningFact[] = []; // Facts that the user has to learn
+  reviewFacts: LearningFact[] = []; // Facts that the user has already learned and needs to review
   showQuestion: boolean = true;
-  session:boolean=false;
-  i:number=0;
+  learningSession: boolean = false;
+  reviewSession: boolean = false;
+  i: number = 0;
   timer: any;
   timeSpent: number = 0;
   learningLength: number = 0;
 
 
-  constructor(private route: ActivatedRoute, private httpClient: HttpClient) {}
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) {
+  }
 
   ngOnInit() {
     this.getPackage();
@@ -40,11 +44,17 @@ export class LearningFactPageComponent implements OnInit, OnDestroy{
     });
   }
 
-  getFacts(){
-    this.httpClient.get(`/api/learningFact/${this.learningPackage.packageId}`).subscribe({
-      next: (res:any) => {
-        this.learningFacts = res.LearningFacts;
-        this.learningLength = res.LearningLength;
+  getFacts() {
+    this.httpClient.get(`/api/allLearningFact/${this.learningPackage.packageId}`).subscribe({
+      next: (res: any) => {
+        this.facts = res;
+        this.learningLength = this.facts.length;
+        this.learningFacts = this.facts.filter(fact => {
+          return fact.confidenceLevel < 4 && new Date(fact.factNextReviewDate) <= new Date();
+        });
+        this.reviewFacts = this.facts.filter(fact => {
+          return fact.confidenceLevel === 4 && new Date(fact.factNextReviewDate) <= new Date();
+        });
       },
       error: (err) => {
         console.error(`Failed to fetch data for package ID ${this.learningPackage.packageId}`, err);
@@ -68,96 +78,114 @@ export class LearningFactPageComponent implements OnInit, OnDestroy{
   }
 
   changeAnswer() {
-    this.showQuestion=!this.showQuestion;
+    this.showQuestion = !this.showQuestion;
   }
 
   nextFact(difficulty: string) {
-    this.updateFact(this.learningFacts[this.i],difficulty);
-    this.putFact(this.learningFacts[this.i]);
-
-
-    if(this.i<this.learningFacts.length-1) {
-      this.i = this.i + 1;
-      if(!this.showQuestion) {
-        this.showQuestion=true;
-      }
+    if (this.learningSession) {
+      this.updateLearningFact(this.learningFacts[this.i], difficulty);
+      this.putFact(this.learningFacts[this.i]);
+    } else {
+      this.updateReviewFact(this.reviewFacts[this.i], difficulty);
+      this.putFact(this.reviewFacts[this.i]);
     }
-    else
-    {
-      console.log("fini");
-      this.session=false;
+
+    if (this.i < (this.learningSession ? this.learningFacts.length - 1 : this.reviewFacts.length - 1)) {
+      this.i++;
+      if (!this.showQuestion) {
+        this.showQuestion = true;
+      }
+    } else {
+      this.learningSession = false;
+      this.reviewSession = false;
       this.getPackage();
       //TODO: METTRE A JOUR LE PACKAGE (PROGRESS)
       //TODO : METTRE MODIFICATION STATS
       clearTimeout(this.timer);
     }
-
-    let value:string = difficulty;
-  //appeller une fonction pour modifier la next date et le level confidence en fonction de la difficulté
   }
 
-  startSession() {
+  startlearningSession() {
     this.startTimer()
-    this.session=true;
-    this.i=0;
+    this.learningSession = true;
+    this.reviewSession = false;
+    this.i = 0;
   }
 
-  startTimer(){
+  startReviewSession() {
+    this.startTimer()
+    this.reviewSession = true;
+    this.learningSession = false;
+    this.i = 0;
+  }
+
+  startTimer() {
     this.timer = setInterval(() => {
       this.timeSpent++;
     }, 1000);
   }
 
-  isButtonDisabled():boolean {
+  isLearningButtonDisabled(): boolean {
     return this.learningFacts.length === 0;
   }
 
-  private updateFact(fact: LearningFact,difficulty :string) {
-    let value=new Date();
+  isReviewButtonDisabled(): boolean {
+    return this.reviewFacts.length === 0;
+  }
+
+  private updateLearningFact(fact: LearningFact, difficulty: string) {
+    let value = new Date();
     switch (difficulty) {
       case "Easy":
-        this.learningFacts[this.i].confidenceLevel = 4;
+        fact.confidenceLevel = 4;
         value.setDate(value.getDate() + 4);
-        this.learningFacts[this.i].factNextReviewDate=value;
+        fact.factNextReviewDate = value;
         break;
       case "Review":
-          this.learningFacts[this.i].confidenceLevel =1;
-          this.learningFacts[this.i].factNextReviewDate=value;
+        fact.confidenceLevel = 1;
+        fact.factNextReviewDate = value;
         break;
       case "Correct":
-        this.learningFacts[this.i].confidenceLevel++;
-        if(this.learningFacts[this.i].confidenceLevel == 4)
-        {
+        fact.confidenceLevel++;
+        if (fact.confidenceLevel == 4) {
           value.setDate(value.getDate() + 1);
-          this.learningFacts[this.i].factNextReviewDate=value;
-        }
-        else {
+          fact.factNextReviewDate = value;
+        } else {
           value.setMinutes(value.getMinutes() + 10);
-          this.learningFacts[this.i].factNextReviewDate=value;
+          fact.factNextReviewDate = value;
         }
         break;
-      default:
-        console.log('no changes');
+    }
+    fact.factTimesReviewed = fact.factTimesReviewed + 1
+    fact.factLastReviewedDate = new Date();
+  }
+
+  private updateReviewFact(fact: LearningFact, difficulty: string) {
+    let dateNow = new Date();
+    let newDate = new Date();
+    let timeBetweenInMiliseconds = new Date(fact.factNextReviewDate).getTime() - new Date(fact.factLastReviewedDate).getTime();
+    let timeBetweenInDays = Math.round(timeBetweenInMiliseconds / (1000 * 3600 * 24));
+
+    switch (difficulty) {
+      case 'Review':
+        if (timeBetweenInDays > 1) {
+          newDate.setDate(dateNow.getDate() + timeBetweenInDays - 1);
+        } else {
+          newDate.setDate(dateNow.getDate() + 1);
+        }
+        break;
+      case 'Hard':
+        newDate.setDate(dateNow.getDate() + timeBetweenInDays + 2);
+        break;
+      case 'Correct':
+        newDate.setDate(dateNow.getDate() + timeBetweenInDays + 5);
+        break;
+      case 'Easy':
+        newDate.setDate(dateNow.getDate() + timeBetweenInDays + 10);
         break;
     }
-    this.learningFacts[this.i].factTimesReviewed = this.learningFacts[this.i].factTimesReviewed+1
-    this.learningFacts[this.i].factLastReviewedDate = new Date();
-    /*
-    if(again) {
-      this.learningFacts[this.i].factNextReviewDate =new Date();
-      console.log(this.learningFacts[this.i].factNextReviewDate)
-    }
-    else
-    {
-      let lastdate =new Date(this.learningFacts[this.i].factLastReviewedDate);
-      let nextdate =new Date(this.learningFacts[this.i].factNextReviewDate);
-      let timeBetween=nextdate.getTime()-lastdate.getTime();
-      timeBetween=timeBetween/(1000*3600*24);
-      console.log(timeBetween)
-       if(timeBetween<=0) timeBetween=1;
-      let value=new Date();
-      value.setTime(value.getTime() + 24*60*60*1000*timeBetween*this.learningFacts[this.i].confidenceLevel);
-      this.learningFacts[this.i].factNextReviewDate =value;
-    }*/
+    fact.factNextReviewDate = newDate;
+    fact.factLastReviewedDate = dateNow;
+    fact.factTimesReviewed++;
   }
 }
