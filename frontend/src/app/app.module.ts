21a8cb71d249005b2e0c7ad2eff25e3927c1034e
mathiasbalian <mathias.balian92@gmail.com>
2023-12-23 22:14:54 +0100

    added declaration for StatisticsPageComponent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from "@angular/common/http";
-import { NavbarComponent } from './navbar/navbar.component';
-import { StudyPageComponent } from './study-page/study-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { LearningPackageCardComponent } from './learning-package-card/learning-package-card.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {StudyPageComponent} from './study-page/study-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {LearningPackageCardComponent} from './learning-package-card/learning-package-card.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatCheckboxModule} from "@angular/material/checkbox";
-import { LearningFactPageComponent } from './learning-fact-page/learning-fact-page.component';
+import {LearningFactPageComponent} from './learning-fact-page/learning-fact-page.component';
 import {FormsModule} from "@angular/forms";
+import {StatisticsPageComponent} from "./statistics-page/statistics-page.component";
 
 @NgModule({
   declarations: [
@@ -24,21 +25,23 @@ import {FormsModule} from "@angular/forms";
     StudyPageComponent,
     HomePageComponent,
     LearningPackageCardComponent,
-    LearningFactPageComponent
+    LearningFactPageComponent,
+    StatisticsPageComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NgbModule,
-        HttpClientModule,
-        MatCardModule,
-        MatButtonModule,
-        MatIconModule,
-        MatProgressBarModule,
-        MatCheckboxModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatCheckboxModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
