0d30f0ee3a2dfb4cc0e0ae5e18c89da4ca601752
mathiasbalian <mathias.balian92@gmail.com>
2023-12-24 12:26:50 +0100

    created logic to generate statistics chart

--- frontend/src/app/statistics-page/statistics-page.component.ts
+++ frontend/src/app/statistics-page/statistics-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {LearningPackage} from "../app.component";
+import {LearningPackage, Statistics} from "../app.component";
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-statistics-page',
@@ -9,14 +10,62 @@ import {LearningPackage} from "../app.component";
 })
 export class StatisticsPageComponent implements OnInit {
 
+  startedLearningPackages: LearningPackage[] = [];
+  statisticsStartedLearningPackages: Statistics[] = [];
+  chartOptions: {} = {};
+
   constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit(): void {
-
+    this.getStartedPackages();
+    this.loadChart();
   }
 
-  getPackages() {
-    this.httpClient.get<LearningPackage[]>("/api/learningPackageF")
+  getStartedPackages() {
+    this.httpClient.get<LearningPackage[]>("/api/startedLearningPackage").subscribe({
+      next: (res: LearningPackage[]) => {
+        this.startedLearningPackages = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  loadChart() {
+    let categories = this.startedLearningPackages.map(learningPackage => (
+      {packageName: learningPackage.packageName}
+    ));
+
+    this.chartOptions = {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Confidence counts by packages'
+      },
+      xAxis: {
+        categories: categories
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Goals'
+        }
+      },
+      legend: {
+        reversed: true
+      },
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          dataLabels: {
+            enabled: true
+          }
+        }
+      }
+    }
+    Highcharts.chart('chart-container', this.chartOptions);
   }
 }
