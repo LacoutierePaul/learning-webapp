0886fd639f2dc84fc3ea61362651ce4a9409e6df
mathiasbalian <mathias.balian92@gmail.com>
2023-12-24 12:34:49 +0100

    added route to get statistics of started learning packages

--- backend/routes/StatisticsRoutes.ts
+++ backend/routes/StatisticsRoutes.ts
@@ -1,7 +1,6 @@
 import * as express from 'express';
-import { Request, Response} from 'express';
-import {Statistics} from "../database/Models"
-
+import {Request, Response} from 'express';
+import {LearningFact, LearningPackage, Statistics} from "../database/Models"
 
 
 const statisticsRoutes = express.Router();
@@ -11,7 +10,7 @@ statisticsRoutes.get("/api/statistic/", async (req: Request, res: Response) => {
     try {
         let stat: Statistics[] = await Statistics.findAll();
         res.status(200).send(stat);
-    } catch(error){
+    } catch (error) {
         res.status(500).send("Could not query the database");
     }
 });
@@ -21,11 +20,33 @@ statisticsRoutes.get("/api/statistic/:id", async (req: Request, res: Response) =
     try {
         const id = +req.params.id;
         let stat: Statistics = await Statistics.findOne({
-            where: { packageId: id }
+            where: {packageId: id}
         });
         res.status(200).send(stat);
-    } catch(error){
+    } catch (error) {
         res.status(500).send("Could not query the database");
+    }
+});
+
+
+statisticsRoutes.get("/api/statisticsStartedPackages", async (req: Request, res: Response) => {
+    try {
+        let packagesWithProgress: LearningPackage[] = await LearningPackage.findAll({
+            where: {
+                packageProgress: {
+                    gt: 0
+                }
+            }
+        });
+        let packageIds: number[] = packagesWithProgress.map(learningPackage => learningPackage.packageId);
+        let statistics: Statistics[] = await Statistics.findAll({
+            where: {
+                packageId: packageIds
+            }
+        });
+        res.status(200).send(statistics);
+    } catch (error) {
+        res.status(500).send(error);
     }
 });
 
@@ -42,7 +63,7 @@ statisticsRoutes.put("/api/statistic/", async (req: Request, res: Response) => {
     try {
         let id = +req.body.packageId
         let stat: Statistics = await Statistics.findOne({
-            where: { packageId: id }
+            where: {packageId: id}
         });
         if (stat) {
             stat.set(req.body);
