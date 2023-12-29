3c4f11a62818ece96297b04a0cacc787e5a1b3d3
mathiasbalian <mathias.balian92@gmail.com>
2023-12-29 14:57:46 +0100

    added a route to delete all facts from a specific package

--- backend/routes/LearningFactRoutes.ts
+++ backend/routes/LearningFactRoutes.ts
@@ -1,8 +1,7 @@
 import * as express from 'express';
-import { Request, Response} from 'express';
+import {Request, Response} from 'express';
 import {LearningFact} from "../database/Models"
 import learningPackageRoutes from "./LearningPackageRoutes";
-
 
 
 const learningFactRoutes = express.Router();
@@ -11,7 +10,7 @@ learningFactRoutes.get("/api/learningFact", async (req: Request, res: Response) 
     try {
         let LearningFacts: LearningFact[] = await LearningFact.findAll();
         res.status(200).send(LearningFacts);
-    } catch(error){
+    } catch (error) {
         res.status(500).send("Could not query the database");
     }
 });
@@ -21,16 +20,14 @@ learningFactRoutes.get("/api/allLearningFact/:id", async (req: Request, res: Res
     try {
         const id = +req.params.id;
         let LearningFacts: LearningFact[] = await LearningFact.findAll({
-            where: { packageId: id }
+            where: {packageId: id}
         });
-        if(LearningFacts) {
+        if (LearningFacts) {
             res.status(200).send(LearningFacts);
+        } else {
+            res.status(404).send({error: 'Facts entity not found for ID: ' + id});
         }
-        else
-        {
-            res.status(404).send({ error: 'Facts entity not found for ID: ' + id });
-        }
-    } catch(error){
+    } catch (error) {
         res.status(500).send("Could not query the database");
     }
 });
@@ -41,20 +38,19 @@ learningFactRoutes.get("/api/learningFact/:id", async (req: Request, res: Respon
         const now = new Date();
         const id = +req.params.id;
         let LearningFacts: LearningFact[] = await LearningFact.findAll({
-            where: { packageId: id }
+            where: {packageId: id}
         });
 
         let LearningFactsFiltered = LearningFacts.filter((fact) => {
             const factNextReviewDate = new Date(fact.factNextReviewDate);
-            return factNextReviewDate <= now && fact.confidenceLevel<4;});
-        if(LearningFacts) {
-            res.status(200).send({'LearningFacts': LearningFactsFiltered,'LearningLength': LearningFacts.length});
+            return factNextReviewDate <= now && fact.confidenceLevel < 4;
+        });
+        if (LearningFacts) {
+            res.status(200).send({'LearningFacts': LearningFactsFiltered, 'LearningLength': LearningFacts.length});
+        } else {
+            res.status(404).send({error: 'Facts entity not found for ID: ' + id});
         }
-        else
-        {
-            res.status(404).send({ error: 'Facts entity not found for ID: ' + id });
-        }
-    } catch(error){
+    } catch (error) {
         res.status(500).send("Could not query the database");
     }
 });
@@ -63,21 +59,20 @@ learningFactRoutes.get("/api/learningFact/:id/count", async (req: Request, res: 
     try {
         const id = +req.params.id;
         let LearningFacts: LearningFact[] = await LearningFact.findAll({
-            where: { packageId: id }
+            where: {packageId: id}
         });
         const now = new Date();
 
         let LearningFactsFiltered = LearningFacts.filter((fact) => {
             const factNextReviewDate = new Date(fact.factNextReviewDate);
-            return factNextReviewDate <= now});
-        if(LearningFactsFiltered) {
+            return factNextReviewDate <= now
+        });
+        if (LearningFactsFiltered) {
             res.status(200).send(String(LearningFactsFiltered.length));
+        } else {
+            res.status(404).send({error: 'Facts entity not found for ID: ' + id});
         }
-        else
-        {
-            res.status(404).send({ error: 'Facts entity not found for ID: ' + id });
-        }
-    } catch(error){
+    } catch (error) {
         res.status(500).send("Could not query the database");
     }
 });
@@ -89,32 +84,29 @@ learningFactRoutes.get("/api/learningFact/:idPackage/:idFact", async (req: Reque
         const idFact = +req.params.idFact;
 
         let LearningFacts: LearningFact = await LearningFact.findOne({
-            where: { packageId: idPackage, factId: idFact}
+            where: {packageId: idPackage, factId: idFact}
         });
-        if(LearningFacts) {
+        if (LearningFacts) {
             res.status(200).send(LearningFacts);
+        } else {
+            res.status(404).send({error: 'Fact entity not found for those ID , package : ' + idPackage + " fact : " + idFact});
         }
-        else
-        {
-            res.status(404).send({ error: 'Fact entity not found for those ID , package : ' + idPackage +" fact : "+idFact });
-        }
-    } catch(error){
+    } catch (error) {
         res.status(500).send("Could not query the database");
     }
 });
 
 learningFactRoutes.post("/api/learningFact", async (req: Request, res: Response) => {
     try {
-        const newLearningFact:LearningFact= await LearningFact.create(req.body);
+        const newLearningFact: LearningFact = await LearningFact.create(req.body);
         res.status(200).send(newLearningFact);
-    } catch(error) {
+    } catch (error) {
         res.status(500).send(error);
     }
 });
 
 
-learningFactRoutes.delete("/api/learningFact/:idPackage/:idFact", async (req: Request, res: Response) =>
-{
+learningFactRoutes.delete("/api/learningFact/:idPackage/:idFact", async (req: Request, res: Response) => {
     try {
         const idPackage = +req.params.idPackage;
         const idFact = +req.params.idFact;
@@ -133,26 +125,39 @@ learningFactRoutes.delete("/api/learningFact/:idPackage/:idFact", async (req: Re
     }
 });
 
+
+learningFactRoutes.delete("/api/learningFact/:packageId", async (req: Request, res: Response) => {
+    try {
+        let packageId: number = +req.params.packageId;
+        await LearningFact.destroy({
+            where: {packageId: packageId}
+        });
+        res.status(200).send({statusMessage: "Facts deleted"});
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+
 learningFactRoutes.put("/api/learningFact", async (req: Request, res: Response) => {
-    try{
+    try {
         let packageId = +req.body.packageId
         let factId = +req.body.factId
         let learningFact: LearningFact = await LearningFact.findOne({
             where: {packageId: packageId, factId: factId}
         });
 
-        if(learningFact){
+        if (learningFact) {
             learningFact.set(req.body);
             await learningFact.save();
             res.status(200).send(learningFact);
         } else {
             res.status(404).send({error: 'Fact entity not found for those ID , package : ' + packageId + " fact : " + factId});
         }
-    } catch(error){
+    } catch (error) {
         res.status(500).send(error);
     }
 });
-
 
 
 export default learningFactRoutes;
