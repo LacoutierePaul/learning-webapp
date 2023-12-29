5e473d7359918daf621ebf74d429b66b0b256286
hectormm1234 <hector.mell24@gmail.com>
2023-12-29 17:13:00 +0100

    Add the .delete() to the StatisticsRoutes.ts and LearningPackageRoutes.ts. Continue to implement the delete-learning-package.component.ts

--- frontend/src/app/delete-learning-package/delete-learning-package.component.ts
+++ frontend/src/app/delete-learning-package/delete-learning-package.component.ts
@@ -4,7 +4,8 @@ import {LearningPackage, LearningFact} from '../app.component'; // Adjust import
 
 @Component({
   selector: 'app-delete-learning-package',
-  templateUrl: './delete-learning-package.component.html'
+  templateUrl: './delete-learning-package.component.html',
+  styleUrls: ['./delete-learning-package.component.css']
 })
 export class DeleteLearningPackageComponent implements OnInit {
   packages: LearningPackage[] = [];
